804ff4ffd513c31e65c48b41444e7c69bfd9ada6
Priya X. Pramesi <ppramesi@visi8.com>
2021-11-09 19:05:03 +0700

    fixed fabelio tour

--- src/pages/panorama/FabelioTour.vue
+++ src/pages/panorama/FabelioTour.vue
@@ -1,3 +1,3 @@
 <template>
-    <iframe class="w-screen h-screen" frameBorder="0" allow="magnetometer; vr; xr; accelerometer; magnetometer; gyroscope; webvr;webxr;" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" scrolling="no" src="https://in3perspective.co.id/3d-model/fabelio-360-tour/fullscreen-nobrand/"></iframe>
+    <iframe class="w-screen h-screen" frameBorder="0" src="https://kuula.co/share/collection/7lwGz" allow="vr; gyroscope; accelerometer; fullscreen" allowfullscreen=""></iframe>
 </template>
